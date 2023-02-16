6aa6bae479dd7e4f1f389bfe1783de74db8b3858
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-02-16 15:53:21 +0000

    Set up profile serializer

--- profiles/admin.py
+++ profiles/admin.py
@@ -7,5 +7,5 @@ class ProfileAdmin(admin.ModelAdmin):
     """
     Admin for Profile model
     """
-    list_display = ('user', 'full_name')
-    search_fields = ['first_name', 'last_name']
+    list_display = ('user', 'full_name', 'email')
+    search_fields = ['user', 'first_name', 'last_name', 'email']
--- profiles/models.py
+++ profiles/models.py
@@ -17,6 +17,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     profile_image = models.ImageField(upload_to='images/', blank=True)
+    email = models.EmailField(blank=True)
 
     def __str__(self):
         return self.user.username
