ff5fa25634837c8cf5aab8da7d04f93bfb247369
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-02-24 17:33:52 +0000

    Add functionality to edit profiles

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 from django.http import Http404
+from rest_framework import status, permissions
 
 
 # Code from CI walkthrough project
@@ -20,6 +21,10 @@ class ProfileList(APIView):
 class ProfileDetail(APIView):
     """
     """
+    serializer_class = ProfileSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -31,3 +36,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
