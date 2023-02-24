fa41ef3b6e08cbce940e8ab0401c3ea50a4e3ee6
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-02-24 17:27:22 +0000

    Add ProfileDetail view

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Project
 
+
 # Code from CI walkthrough project
 class ProjectSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.username')
     is_creator = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='creator.profile.id')
+    profile_image = serializers.ReadOnlyField(
+        source='creator.profile.profile_image.url'
+        )
 
     def get_is_creator(self, obj):
         request = self.context['request']
