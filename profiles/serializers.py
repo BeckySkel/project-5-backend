7a24d54536e0b38c9a3cf82ece1d2cb1ce361572
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-02-24 17:49:34 +0000

    Add authentication and permissions to views

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,9 +4,15 @@ from .models import Profile
 # Code from CI walkthrough project
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    is_user = serializers.SerializerMethodField()
+
+    def get_is_user(self, obj):
+        request = self.context['request']
+        return request.user == obj.user
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'user', 'first_name', 'last_name', 'profile_image', 'email'
+            'id', 'user', 'first_name', 'last_name', 'profile_image', 'email',
+            'is_user'
         ]
