17cd5f7f98d3e0380e092666b0ff9e043a510843
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-02-27 21:05:41 +0000

    Update settings file with correct project name

--- devise/permissions.py
+++ devise/permissions.py
@@ -3,10 +3,8 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        print("lllllllllllllllllllllll")
         if request.method in permissions.SAFE_METHODS:
             return True
-        # return obj.user == request.user
         try:
             return obj.user == request.user
         except AttributeError:
