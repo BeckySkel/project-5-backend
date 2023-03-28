5bfc70bb6bf63b4d57f53dd80ab5a4118ea6b373
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-03-28 23:33:46 +0000

    Add filtering and search to contributor serializer

--- contributors/views.py
+++ contributors/views.py
@@ -11,6 +11,26 @@ class ContributorList(generics.ListCreateAPIView):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
 
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+
+    filterset_fields = [
+        # Contributor instances of the selected project
+        'project',
+        # Contributor invites sent by the selected user
+        'creator__profile',
+        # Contributor instances of the selected user
+        'user__profile'
+    ]
+
+    search_fields = [
+        'user',
+        'project',
+        'creator'
+    ]
+
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
 
